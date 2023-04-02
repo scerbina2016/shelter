6e9ddcd6637c25e6645b6186ab12c6ed8728d3f3
Andrey Scerbina <scerbina2016@gmail.com>
2023-04-02 17:18:25 +0300

    fix:nav open dis

--- script.js
+++ script.js
@@ -13,13 +13,11 @@ let smallOpen = () => {
 
 closeBurger.addEventListener('click',()=> {
     menuOpened = false;
-    console.log('in close burger'+menuOpened);
     dropDownBlock.style.width = '0px';
 });
 
 dropDownBlock.addEventListener('click',()=> {
     menuOpened = false;
-    console.log('in close '+menuOpened);
     dropDownBlock.style.width = '0px';
 });
 
@@ -35,28 +33,3 @@ drop.addEventListener('click',() =>{
     console.log('in open  ' + menuOpened)
     smallOpen();
 });
-
-
-
-/*window.addEventListener('resize',()=>{
-    console.log('in resize  '+menuOpened);
-    if(menuOpened){
-        if( window. innerWidth > 321) {
-            dropDownBlock.style.width = '320px';
-        } else {
-            dropDownBlock.style.width = '100%';
-        }
-    }
-});
-
-drop.addEventListener('click',() =>{
-    menuOpened = true;
-    console.log('in open  ' + menuOpened)
-    if( window. innerWidth > 321) {
-        dropDownBlock.style.width = '320px';
-    } else {
-        dropDownBlock.style.width = '100%';
-    }
-});*/
-
-
